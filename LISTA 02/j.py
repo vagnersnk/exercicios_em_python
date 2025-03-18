0519ea91c3e527601036e324b401db6398e99c8b
vagnersnk <58046945+vagnersnk@users.noreply.github.com>
2025-03-17 21:08:54 -0300

    Update j.py

--- LISTA 02/j.py
+++ LISTA 02/j.py
@@ -1,3 +1,3 @@
 # Faça um algoritmo para calcular o MMC de dois números.
 # (MMC é o menor múltiplo comum entre os dois números.
-# Note que um dos dois números já pode ser o MMC)  
+# Note que um dos dois números já pode ser o MMC)
